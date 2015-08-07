c738ee26c01f83fd0676499b6e8b142386b8375b
Jassim Talat <jassim.talat@gmail.com>
2015-08-07 18:42:32 +0500

    commit
    commit

---
+++ js/osx.js
@@ -0,0 +1,60 @@
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("input.osx, a.osx").click(function (e) {
+				e.preventDefault();	
+
+				$("#osx-modal-content").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 100,
+					opacity: 30, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					onClose: OSX.close
+				});
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('fast', function () {
+				$("#osx-modal-content", self.container).show();
+				var title = $("#osx-modal-title", self.container);
+				title.show();
+				d.container.slideDown('fast', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							10,
+							function () {
+								$("div.close", self.container).show();
+								$("#osx-modal-data", self.container).show();
+							}
+						);
+					}, 0);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				50,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
